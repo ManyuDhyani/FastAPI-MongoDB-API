7659300fef4fe9b6ffa78b46ed9cad9110b413b6
ManyuDhyani <manyudhyani@gmail.com>
2024-07-19 19:26:23 -0400

    Routes and API

---
+++ routes/route.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from models.todos import Todo
+from config.database import collection_name
+from schema.schemas import list_serial
+from bson import ObjectId
+
+
+router = APIRouter()
+
+
+@router.get("/")
+async def get_todos():
+    todos = list_serial(collection_name.find())
+    return todos
+
+@router.post("/")
+async def post_todo(todo: Todo):
+    collection_name.insert_one(dict(todo))
